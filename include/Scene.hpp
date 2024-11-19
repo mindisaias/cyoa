4d505ce9df8e5f35204d28717128f7e32a95951e
nander100 <atota002@xe-01.cs.ucr.edu>
2024-11-18 20:02:18 -0800

    Final implementation fot the scenes and choices class. Added function stubs for the Gamerunner class. Added test cases for the scene class. Runs clean in Valgrind.

--- include/Scene.hpp
+++ include/Scene.hpp
@@ -3,21 +3,25 @@
 
 #include "Choice.hpp"
 #include <vector>
-
+#include <iostream>
+#include <string>
+using namespace std;
 class Scene {
     public:
         Scene(): prompt("") {}
-        Scene(string p): prompt(p) {}
-        
-        string prompt;
-        vector<Choice> choices;
-        
-        void editPrompt();
-        void editChoice();
-        void display() const;
+        Scene(const string p, const string n): prompt(p), sceneName(n) {}
+        ~Scene();
+        void editPrompt(const string p) ;
+        int editChoice(int, Choice);
+        void addChoice(Choice*);
+        void display(ostream&) const;
+        string getSceneName() const { return sceneName; }
+        string getResultScene(int) const; // takes in the index of the choice and returns the name of the scene
+        void setSceneName(const string n) { this->sceneName = n; }
     private:
-        int sceneID;
+        string prompt;
+        vector<Choice*> choices;
+        string sceneName;
 };
 
-
-#endif
+#endif 
