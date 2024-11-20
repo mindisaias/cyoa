a1898da9f9465033e0d51e219cd6686b37ede80a
ImmortalWave <inactivegoat@gmail.com>
2024-11-20 00:00:44 -0800

    added basic funcitonality to GameCreator class

--- include/GameCreator.hpp
+++ include/GameCreator.hpp
@@ -2,20 +2,36 @@
 #define GAME_CREATOR_HPP
 
 #include "Game.hpp"
+#include <map> 
+#include <limits>
+
+using namespace std;
 
 class GameCreator {
     public:
-        void chooseScene();
+        //helpers
+        void start();
+        void printMenu();
+        //functions for game
         void addScene();
-        void editScecne();
-        void removeScene();
-        void publish();
-        void saveScene();
-        void chooseGame();
+        //void editScene();
+        //void addChoice();
+        void printSceneNames();
+        //void chooseScene();
+        //void removeScene();
+        //void publish();
+        //void saveScene();
+        //void chooseGame();
         
     private:
+        void addScene(const string & prompt, const string & name);
+
+
+
+
         Scene currScene;
         Game currGame;
+
 };
 
 #endif
--- src/GameCreator.cpp
+++ src/GameCreator.cpp
@@ -1,2 +1,84 @@
 #include "../include/GameCreator.hpp"
+#include <iostream> 
+using namespace std;
+
+
+void clearTerminal() {
+    for (int i = 0; i < 100; i++) {
+        cout << '\n';
+    }
+}
+
+void GameCreator::printMenu() {
+    clearTerminal();
+    cout << "1. Add Scene\n";
+    cout << "2. Edit Scene\n";
+    cout << "3. Remove Scene\n";
+    cout << "4. Print Scene Names\n";
+    cout << "5. Quit\n";
+
+    
+
+}
+
+
+void GameCreator::start(){
+
+    int choice = 0;
+    do{
+
+        if(choice == 1) {
+            //add scene
+        }
+        else if(choice == 2) {
+
+        }
+        else if(choice == 3) {
+
+        }
+        else if(choice == 4) {
+
+        }
+        else if(choice == 5) {
+
+        }
+        else {
+            
+        }
+
+
+        cin >> choice;
+    }while(choice)
+
+}
+void GameCreator::addScene() {
+
+    string name;
+    string prompt;
+    cout << "Enter the name of your scene (One Word): "; 
+    cin >> name;
+    
+    cout << "Enter the prompt for scene \"" << name << "\":\n";
+
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    getline(cin,prompt);
+
+    addScene(prompt,name);
+
+}
+void GameCreator::addScene(const string & prompt,const string & name) {
+
+    Scene * newScene = new Scene(prompt,name);
+
+    currGame.gameScenes[name] = newScene;
+
+}
+
+void GameCreator::printSceneNames() {
+    int i = 1;
+    for(const auto & sceneName : currGame.gameScenes) {
+        cout << i << ". "<< sceneName.first << "\n";
+        i++;
+    }
+}
 
