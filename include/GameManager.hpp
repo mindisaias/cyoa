2ea7efa365990e1b3fc6b2da6e83588faebb24c2
nander100 <atota002@xe-01.cs.ucr.edu>
2024-11-21 17:21:14 -0800

    added function stubs for log in and create account. made game manager destructor and started

--- include/GameManager.hpp
+++ include/GameManager.hpp
@@ -8,19 +8,33 @@
 #include "User.hpp"
 #include <map>
 
+// SCREEN NAME DEF
+#define WELCOME_NAME          "WP"
+#define SIGNIN_NAME           "SI"
+#define CREATE_ACCOUNT_NAME   "CA"
+#define MAIN_MENU_NAME        "MM"
+#define LIST_GAMES_NAME       "LG"
+#define PLAY_GAMES_NAME       "PG"
+#define CREATE_GAMES_NAME     "CG"
+
 class GameManager {
     private:
         Game currGame;
+        
         //TODO: add database
         User currUser;
         GameCreator GC;
         GameRunner GR;
-        map<string, Scene> screnes; 
+        map<string, Scene*> screens; 
+        Scene* currentScene;
 
         // HELPER FUNCTIONS
-        void loadScrenes(); // loads all of the default screnes         
+        void loadScreens(); // loads all of the default screens
+        User signin() const; // returns the name of the user that was signed in
+        User createAccount();
     public:
         void run();
+        ~GameManager();
 };
 
 
--- src/GameManager.cpp
+++ src/GameManager.cpp
@@ -1,36 +1,94 @@
 #include "../include/GameManager.hpp"
 
-void GameManager::loadScrenes() {
-    // Scene declaration
-    Scene* welcomePage = new Scene();
-    welcomePage->setSceneName("WP");
+GameManager::~GameManager() {
+    for(const auto& it : screens) {
+        delete screens[it.first];
+    }
+}
 
-    Scene* signIn = new Scene();
-    signIn->setSceneName("SI");
+User GameManager::signin() const { // MOCK FUNCTION
+    User newUser;
+    newUser.username = "nander100";
+    newUser.password = "password";
+    return newUser;
+}
 
-    Scene* createAccount = new Scene();
-    createAccount->setSceneName("CA");
+User GameManager::createAccount() { // MOCK FUNCTION
+    User newUser;
+    newUser.username = "nander100";
+    newUser.password = "password";
+    return newUser;
+}
 
-    Scene* mainMenu = new Scene();
-    mainMenu->setSceneName("MM");
-
-    // ADD SCENE OPTIONS
-    // Welcome page
-    welcomePage->editPrompt("Welcome to Make Your Own Adventure (MYOA)!");
-    welcomePage->addChoice(new Choice("Sign-In", "SI"));
-    welcomePage->addChoice(new Choice("Create Account", "CA"));
-
-    // Sign-in
-    // Create Account
-    // Main Menu
-    mainMenu->editPrompt("What would you like to do?");
+void GameManager::loadScreens() { 
+    // welcome
+    screens[WELCOME_NAME] = new Scene();
+    screens[WELCOME_NAME]->setSceneName(WELCOME_NAME);
+    screens[WELCOME_NAME]->editPrompt("Welcome to Make Your Own Adventure (MYOA)!");
+    screens[WELCOME_NAME]->addChoice(new Choice("Sign-In", SIGNIN_NAME));
+    screens[WELCOME_NAME]->addChoice(new Choice("Create Account", CREATE_ACCOUNT_NAME));
     
+    // sign in page
+    screens[SIGNIN_NAME] = new Scene();
+    screens[SIGNIN_NAME]->setSceneName(SIGNIN_NAME);
+    screens[SIGNIN_NAME]->addChoice(new Choice("To Main Menu", MAIN_MENU_NAME));
 
-    
+    // create account
+    screens[CREATE_ACCOUNT_NAME] = new Scene();
+    screens[CREATE_ACCOUNT_NAME]->setSceneName(CREATE_ACCOUNT_NAME);
+    screens[CREATE_ACCOUNT_NAME]->addChoice(new Choice("To Main Menu", MAIN_MENU_NAME));
 
+    // main menu
+    screens[MAIN_MENU_NAME] = new Scene();
+    screens[MAIN_MENU_NAME]->setSceneName(MAIN_MENU_NAME);
+    screens[MAIN_MENU_NAME]->editPrompt("What would you like to do?");
+    screens[MAIN_MENU_NAME]->addChoice(new Choice("Play a user created game", MAIN_MENU_NAME));
+    screens[MAIN_MENU_NAME]->addChoice(new Choice("Start/resume game editing", CREATE_GAMES_NAME));
+
+    //play game
+    screens[PLAY_GAMES_NAME] = new Scene();
+    screens[PLAY_GAMES_NAME]->setSceneName(PLAY_GAMES_NAME);
+    screens[PLAY_GAMES_NAME]->editPrompt("Selecte a game to play");
+    // TODO: choices to be added later afte the user signs in 
 
 }
 
-void GameManager::run() {
 
+void GameManager::run() {
+    loadScreens(); // Initialize Screnes
+
+    // placeholder variable for scene transitions
+    string nextSceneName; 
+    int userDecision;
+
+    // starting screen = welcome page 
+    currentScene = screens[WELCOME_NAME];
+    currentScene->display(cout); // prints out the options
+    cin >> userDecision;
+    nextSceneName = currentScene->getResultScene(userDecision);
+    while(nextSceneName == "") { // checks for valid input
+        cout << "Invalid choice. Select a valid option." << endl;
+        currentScene->display(cout);
+        cin >> userDecision;
+        nextSceneName = currentScene->getResultScene(userDecision);
+    }
+
+    // sign in or create an account function calls
+    if(nextSceneName == SIGNIN_NAME)
+        currUser = signin();
+    else if(nextSceneName == CREATE_ACCOUNT_NAME)
+        currUser = createAccount();
+
+    // Once the user is signed in display the main menu 
+    currentScene = screens[MAIN_MENU_NAME];
+    cout << "Welcome " << currUser.username << "! ";
+    currentScene->display(cout);
+    cin >> userDecision;
+    nextSceneName = currentScene->getResultScene(userDecision);
+    
+    // TODO: fill a string list with all the possible playable games from the database
+    // TODO: create 
+
+
+    // create a scene to display the games
 }
