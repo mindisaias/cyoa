3472613408e29826aa8e14188e6039ca40f67440
noodlespad <lynvychang@gmail.com>
2024-11-25 17:02:03 -0800

    Game class edited with constructors

--- include/Game.hpp
+++ include/Game.hpp
@@ -5,6 +5,8 @@
 
 class Game {
     public:
+        Game(): title("") , author(""), description (""), gameID(-1) {}
+        Game(string t, string a, string d, int ID): title(t), author(a), description(d), gameID(ID) {}
         string title;
         string author;
         string description;
