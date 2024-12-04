5b0c5d26fefde1419bfcbe585f9792877b25fa7a
nander100 <atota002@xe-01.cs.ucr.edu>
2024-12-04 00:35:12 -0800

    minor fixes

--- include/User.hpp
+++ include/User.hpp
@@ -1,6 +1,6 @@
 #ifndef USER_HPP
 #define USER_HPP
-#include <../sqlite3/sqlite3.h>
+#include "../sqlite3/sqlite3.h"
 #include <string>
 using namespace std;
 
@@ -9,6 +9,7 @@ class User {
         string username;
         string password;
         int id;
+        User() {};
         User(string u, string p): username(u), password(p) {} 
         bool isValidCredentials(string username, string password, sqlite3* db);
         void addUser(string username, string password, sqlite3* db);
